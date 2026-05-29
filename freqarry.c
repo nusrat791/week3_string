fe9324cf4ff040be8558c0d9c77d39106904a6e6
nusrat791 <nusratjahantithi791@gmail.com>
2026-05-29 10:48:50 +0600

    freq

---
+++ freqarry.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+
+int n;
+scanf("%d",&n);
+int a[n];
+for(int i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+int freq[6]={0};
+
+for(int u=0;i<n;i++)
+{
+    if(a[i]==0)
+    {
+        freq[0]++;
+    }
+    else if(a[i]==1)
+    {
+        freq[1]++;
+    }
+    else if(a[i]==2)
+    {
+        freq[2]++;
+    }
+    else if(a[i]==3)
+    {
+        freq[3]++;
+    }
+}
+for(int i=0;i<n;i++)
+{
+    freq[a[i]]++;
+}
+printf("%d->%d\n",0,freq[0]);
+printf("%d->%d\n",1,freq[1]);
+printf("%d->%d\n",2,freq[2]);
+printf("%d->%d\n",3,freq[3]);
+printf("%d->%d\n",4,freq[4]);
+printf("%d->%d\n",5,freq[5]);
+
+
+    return 0;
+}
